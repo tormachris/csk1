1af5ab61f291c1c9087e2a9e7ac01694ed104e4f
Torma Kristóf <kristof10@outlook.com>
2018-03-08 01:44:04 +0100

    Added main

---
+++ src/logic/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package logic;
+
+/**
+ * @author kristof
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
