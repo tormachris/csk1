9386c679a273e8c06f07e669b32aa2e5d5c72b6f
Torma Kristóf <tormakristof@tormakristof.eu>
2018-03-18 21:23:33 +0100

    Maps Logged

--- src/logic/Map.java
+++ src/logic/Map.java
@@ -9,10 +9,9 @@ import java.util.*;
 public class Map implements Steppable {
 	
 	private Integer ticksRemain;
-	private Integer defaultTime;
-	private Set<Crate> crates;
-	private Set<Worker> workers;
-	private Set<Tile> tiles;
+	private Set<Crate> crates; //TODO: ADDER FUNCTION
+	private Set<Worker> workers; //TODO: ADDER
+	private Set<Tile> tiles; //TODO: ADDER
 	
 	private static final int DEFAULTTICKSREMAIN=100000000;
 	
@@ -21,10 +20,15 @@ public class Map implements Steppable {
 	 * Initialises the sets and Integers.
 	 */
 	public Map() {
-		setCrates(new HashSet<Crate>());
-		setWorkers(new HashSet<Worker>());
-		setTiles(new HashSet<Tile>());
-		ticksRemain=Integer.valueOf(DEFAULTTICKSREMAIN);
+		System.out.println("!New instance of Map created.");
+		System.out.println(">[:Map].setCrates(new HashSet<Crate>())");
+		this.setCrates(new HashSet<Crate>());
+		System.out.println(">[:Map].setWorkers(new HashSet<Workers>())");
+		this.setWorkers(new HashSet<Worker>());
+		System.out.println(">[:Map].setTiles(new HashSet<Tile>())");
+		this.setTiles(new HashSet<Tile>());
+		System.out.println(">[:Map].resetTimer()");
+		this.resetTimer();
 	}
 	
 	/**
@@ -32,8 +36,11 @@ public class Map implements Steppable {
 	 */
 	public void startMap()
 	{
+		System.out.println(">[:Map].resetTimer()");
 		this.resetTimer();
+		System.out.println(">Timer.getInstance().addSteppable(this)");
 		Timer.getInstance().addSteppable(this);
+		System.out.println("<[:Map].startMap()");
 	}
 	
 	/**
@@ -41,7 +48,9 @@ public class Map implements Steppable {
 	 */
 	public void endMap()
 	{
+		System.out.println(">Timer.getInstance().removeSteppable(this)");
 		Timer.getInstance().removeSteppable(this);
+		System.out.println("<[:Map].endMap()");
 	}
 	
 	/**
@@ -50,21 +59,24 @@ public class Map implements Steppable {
 	public void resetTimer()
 	{
 		//reseting the time
-		setTicksRemain(defaultTime);
-		return;
+		System.out.println("#ticksRemain=DEFAULTTICKSREMAIN");
+		this.setTicksRemain(DEFAULTTICKSREMAIN);
+		System.out.println("<[:Map].resetTimer()");
 	}
 	
 	/**
 	 * Called by the Timer at every Tick. If there are no crates, only one worker or no more
 	 * time left it will end the map. Else it will shorten the remaining time by one second.
+	 * 
+	 * Not going to log Step() functions, to not spam the STDout.
 	 */
 	public void step()
 	{
 		//checking if there is any reason to end the map
-		if(crates.isEmpty() || workers.size() != 2 || ticksRemain == 0)
+		if(crates.isEmpty() || workers.size() <= 2 || ticksRemain == 0)
 			endMap();
 		else
-			//shortening the remaining time by 1s
+			//shortening the remaining time by 1 tick
 			ticksRemain--;
 		
 		return;
@@ -74,6 +86,7 @@ public class Map implements Steppable {
 	 * @return the tiles
 	 */
 	public Set<Tile> getTiles() {
+		System.out.println("<[:Map].getTiles(): tiles");
 		return tiles;
 	}
 
@@ -81,12 +94,15 @@ public class Map implements Steppable {
 	 * @param tiles the tiles to set
 	 */
 	public void setTiles(Set<Tile> tiles) {
+		System.out.println("#tiles=newtiles");
 		this.tiles = tiles;
+		System.out.println("<[:Map].setTiles(tiles): void");
 	}
 	/**
 	 * @return the ticksRemain
 	 */
 	public Integer getTicksRemain() {
+		System.out.println("<[:Map].getTicksRemain(): " + ticksRemain.toString());
 		return ticksRemain;
 	}
 
@@ -94,27 +110,16 @@ public class Map implements Steppable {
 	 * @param ticksRemain the ticksRemain to set
 	 */
 	public void setTicksRemain(Integer ticksRemain) {
+		System.out.println("#ticksRemain=" + ticksRemain.toString());
 		this.ticksRemain = ticksRemain;
-	}
-
-	/**
-	 * @return the defaultTime
-	 */
-	public Integer getDefaultTime() {
-		return defaultTime;
-	}
-
-	/**
-	 * @param defaultTime the defaultTime to set
-	 */
-	public void setDefaultTime(Integer defaultTime) {
-		this.defaultTime = defaultTime;
+		System.out.println("<[:Map].setTicksRemain(ticksRemain): void");
 	}
 
 	/**
 	 * @return the crates
 	 */
 	public Set<Crate> getCrates() {
+		System.out.println("<[:Map].getCrates(): crates");
 		return crates;
 	}
 
@@ -122,13 +127,16 @@ public class Map implements Steppable {
 	 * @param crates the crates to set
 	 */
 	public void setCrates(Set<Crate> crates) {
+		System.out.println("#crates=newcrates");
 		this.crates = crates;
+		System.out.println("<[:Map].setCrates(crates): void");
 	}
 
 	/**
 	 * @return the workers
 	 */
 	public Set<Worker> getWorkers() {
+		System.out.println("<[:Map].getWorkers(): workers");
 		return workers;
 	}
 
@@ -136,6 +144,8 @@ public class Map implements Steppable {
 	 * @param workers the workers to set
 	 */
 	public void setWorkers(Set<Worker> workers) {
+		System.out.println("#workers=newworkers");
 		this.workers = workers;
+		System.out.println("<[:Map].setWorkers(workers): void");
 	}
 }
