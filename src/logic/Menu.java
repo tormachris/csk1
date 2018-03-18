ea9677554442774122e6d7c49f85efe76b5dceed
Torma Kristóf <tormakristof@tormakristof.eu>
2018-03-18 14:56:25 +0100

    Added menu clas

---
+++ src/logic/Menu.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package logic;
+
+/**
+ * @author krist
+ * This class is the skeleton's menu system.
+ * There is going to be only one instance in the Main class!
+ */
+public class Menu {
+
+	/**
+	 * @author krist
+	 * This method shows the menu. It is the entry point of the menu.
+	 */
+	public void show() {
+		
+		return;
+	}
+	
+}
