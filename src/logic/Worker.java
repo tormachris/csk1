51d3ba129d8ecd7b1239cf0e8607232c90498486
Torma Kristóf <tormakristof@tormakristof.eu>
2018-03-19 12:18:02 +0100

    Fixed some shit before beadas

--- src/logic/Worker.java
+++ src/logic/Worker.java
@@ -8,19 +8,6 @@ public class Worker extends Thing{
 	private Integer points;
 	
 	/**
-	 * This function is called whenever a Worker gets hit by another Worker. As a worker can't 
-	 * move other workers, this Worker won't move.
-	 * @param w  The other worker
-	 * @param d  The direction towards the other worker is moving
-	 * @param o	 The owner of the other worker.
-	 * @return	 False because workers can't be moved by other workers.
-	 */
-	public boolean hitBy(Worker w,Direction d,Thing o)
-	{
-		return true;
-	}
-	
-	/**
 	 * This function is called whenever a Worker gets hit by a Crate. The Crate will take the
 	 * worker's tile anyway so this will return true value however the worker might get 
 	 * squashed.
