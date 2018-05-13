839dc8df517fc7f33a9b6e36f2e3ebf1cc6e89b1
Torma Kristóf <kristof10@outlook.com>
2018-05-13 21:56:40 +0200

    hole now works

--- src/logic/Switch.java
+++ src/logic/Switch.java
@@ -10,70 +10,87 @@ public class Switch extends Tile {
 	 * 
 	 */
 	private static final long serialVersionUID = 8468456817549292315L;
-	private static final Logger LOGGER = Logger.getLogger( Switch.class.getName() );
+	private static final Logger LOGGER = Logger.getLogger(Switch.class.getName());
 	private Hole hole;
+
 	public Switch() {
 		LOGGER.setLevel(Level.ALL);
 		ConsoleHandler handler = new ConsoleHandler();
 		handler.setFormatter(new SimpleFormatter());
 		LOGGER.addHandler(handler);
 		handler.setLevel(Level.ALL);
-		
+
 	}
+
+	@Override
+	public void remove(Thing t) {
+		// checking if the Thing is on this Tile
+		if (t.equals(super.getThing()))
+			hole.setOpen(false);
+		super.remove(t);
+	}
+
 	/**
-	 * Acts like a normal Tile, however if the Thing gets accepted it will call the OnSwitch 
-	 * function of the Thing 
-	 * @param t The Thing that moves onto the Switch
+	 * Acts like a normal Tile, however if the Thing gets accepted it will call the
+	 * OnSwitch function of the Thing
+	 * 
+	 * @param t
+	 *            The Thing that moves onto the Switch
 	 * @return Shows whether the Thing has been accepted
 	 */
 	@Override
-	public boolean accept(Thing t)
-	{
-		//calls the same function of its superclass
+	public boolean accept(Thing t) {
+		// calls the same function of its superclass
 		Boolean accepted = Boolean.valueOf(super.accept(t));
-		//if the Thing is allowed to move onto the Tile the Switch will trigger its OnSwitch()
-		//function in order to inform the Thing that it has moved onto a Switch
-		if(accepted)
+		// if the Thing is allowed to move onto the Tile the Switch will trigger its
+		// OnSwitch()
+		// function in order to inform the Thing that it has moved onto a Switch
+		if (accepted) {
 			t.onSwitch(this);
-		LOGGER.log( Level.FINE, "Switch accepted:{0}",accepted);
+			if (!t.getClass().equals(Crate.class))
+				hole.setOpen(false);
+		}
+		LOGGER.log(Level.FINE, "Switch accepted:{0}", accepted);
 		return accepted.booleanValue();
 	}
-	
+
 	/**
-	 * This function should be called when a Crate moves onto a switch. If this happens the
-	 * Switch will toggle the state of the Hole linked to it.
-	 * @param c The Crate that has moved onto the Switch.
+	 * This function should be called when a Crate moves onto a switch. If this
+	 * happens the Switch will toggle the state of the Hole linked to it.
+	 * 
+	 * @param c
+	 *            The Crate that has moved onto the Switch.
 	 */
-	public void activate()
-	{
-		LOGGER.log( Level.FINE, "Switch toggling hole");
+	public void activate() {
+		LOGGER.log(Level.FINE, "Switch toggling hole");
 		hole.toggleOpen();
 	}
-	
+
 	/**
 	 * Setter of the attribute Hole.
-	 * @param newvalue The new hole
+	 * 
+	 * @param newvalue
+	 *            The new hole
 	 */
-	public void setHole(Hole newvalue)
-	{
-		if(newvalue!=null) {		//Checking for valid value
-			hole=newvalue;
-			LOGGER.log( Level.FINE, "New value for hole set");
-		}
-		else throw new NullPointerException("newvalue is null");
+	public void setHole(Hole newvalue) {
+		if (newvalue != null) { // Checking for valid value
+			hole = newvalue;
+			LOGGER.log(Level.FINE, "New value for hole set");
+		} else
+			throw new NullPointerException("newvalue is null");
 	}
-	
+
 	/**
 	 * Gets the current hole.
 	 */
 	public Hole getHole() {
 		return hole;
 	}
-	
+
 	/**
 	 * Constructor. Sets the hole.
 	 */
-	public Switch(Hole h){
+	public Switch(Hole h) {
 		this.setHole(h);
 	}
 }
